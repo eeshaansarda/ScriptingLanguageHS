796b227702f108c9757e76e20fc1bc75fbc91959
Alastair Choo <ccflaneur@icloud.com>
2021-04-12 10:05:25 +0100

    Commit to stash changes

--- Code/Expr.hs
+++ Code/Expr.hs
@@ -2,8 +2,6 @@ module Expr where
 
 import Parsing
 
-import Data.Maybe
-
 type Name = String
 
 -- At first, 'Expr' contains only addition, conversion to strings, and integer
@@ -13,6 +11,7 @@ data Expr = Add Expr Expr
           | Mul Expr Expr
           | Div Expr Expr
           | ToString Expr
+          | ToInt    Expr
           | Val Value
           | Var Name
           | Concat Expr Expr
@@ -32,24 +31,25 @@ data Value = IntVal Int | FltVal Float | StrVal String
 eval :: [(Name, Value)] -> -- Variable name to value mapping
         Expr -> -- Expression to evaluate
         Maybe Value -- Result (if no errors such as missing variables)
-eval vars (Val x) = Just x -- for values, just give the value directly
-eval vars (Var x) = lookup x vars
+eval vars (Val x)      = Just x -- for values, just give the value directly
+eval vars (Var x)      = lookup x vars
 eval vars (ToString x) = Just (StrVal (show x))
+eval vars (ToInt x)    = Just (IntVal (read x :: Int))
 eval vars (Concat x y) = case (eval vars x, eval vars y) of
-  (Just (StrVal a), Just (StrVal b)) -> Just (StrVal (a ++ b))
-  _ -> Nothing
-eval vars expr = case (eval vars x, eval vars y) of
-  (Just (FltVal f1), Just (FltVal f2)) -> Just (FltVal (func f1 f2))
-  (Just (FltVal f), Just (IntVal i)) -> Just (FltVal (func f (fromIntegral i)))
-  (Just (IntVal i), Just (FltVal f)) -> Just (FltVal (func (fromIntegral i) f))
-  (Just (IntVal i1), Just (IntVal i2)) -> Just (IntVal (round (func (fromIntegral i1) (fromIntegral i2))))
-  _ -> Nothing
-  where
-    (func, x, y) = case expr of
-      Add expr1 expr2 -> ((+), expr1, expr2)
-      Sub expr1 expr2 -> ((-), expr1, expr2)
-      Mul expr1 expr2 -> ((*), expr1, expr2)
-      Div expr1 expr2 -> ((/), expr1, expr2)
+                              (Just (StrVal a), Just (StrVal b)) -> Just (StrVal (a ++ b))
+                              _                                  -> Nothing
+eval vars expr         = case (eval vars x, eval vars y) of
+                              (Just (FltVal f1), Just (FltVal f2)) -> Just (FltVal (func f1 f2))
+                              (Just (FltVal f), Just (IntVal i))   -> Just (FltVal (func f (fromIntegral i)))
+                              (Just (IntVal i), Just (FltVal f))   -> Just (FltVal (func (fromIntegral i) f))
+                              (Just (IntVal i1), Just (IntVal i2)) -> Just (IntVal (round (func (fromIntegral i1) (fromIntegral i2))))
+                              _                                    -> Nothing
+                              where
+                                (func, x, y) = case expr of
+                                  Add expr1 expr2 -> ((+), expr1, expr2)
+                                  Sub expr1 expr2 -> ((-), expr1, expr2)
+                                  Mul expr1 expr2 -> ((*), expr1, expr2)
+                                  Div expr1 expr2 -> ((/), expr1, expr2)
 
 
 digitToInt :: Char -> Int
@@ -64,20 +64,20 @@ pCommand = do t <- identifier
                    space
                    e <- pExpr
                    return (Print e)
-                 ||| do string "quit"
-                        return Quit
+            ||| do string "quit"
+                   return Quit
 
 pExpr :: Parser Expr
-pExpr = (do t <- pTerm
-            do symbol "+"
-               e <- pExpr
-               return (Add t e)
-             ||| do symbol "-"
-                    e <- pExpr
-                    return (Sub t e)
-                  ||| return t)
-        ||| do s <- pStringExpr
-               return s
+pExpr = do t <- pTerm
+           do symbol "+"
+              e <- pExpr
+              return (Add t e)
+            ||| do symbol "-"
+                   e <- pExpr
+                   return (Sub t e)
+                 ||| return t
+         ||| do s <- pStringExpr
+                return s
 
 pFactor :: Parser Expr
 pFactor = do f <- float
@@ -101,10 +101,6 @@ pTerm = do f <- pFactor
                    return (Div f t)
                  ||| return f
 
--- What happens when it doesnt find "
--- Maybe it looks till the end of file
--- and then results in an error?
--- Or can add a functionality for one line strings
 pString :: Parser Expr
 pString = do char '"'
              str <- many (sat (/= '"'))
