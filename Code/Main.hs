a81d4df437d2bd16f7935543ade97ec77e32242f
Zihao Li <zl81@st-andrews.ac.uk>
2021-04-22 17:31:52 +0800

    The first usable version of the new design

--- Code/Main.hs
+++ Code/Main.hs
@@ -1,11 +1,21 @@
 module Main where
 
 import System.Console.Haskeline
+import Data.List (isPrefixOf)
+import Control.Monad.State.Strict (get, runStateT, StateT)
+
 -- import Control.Monad.Trans
 import Parsing
 import Expr
 import REPL
 
-main :: IO ()
-main = runInputT defaultSettings (repl initState)
+-- Copied from https://www.reddit.com/r/haskell/comments/1os0yq/haskeline_woes/
+-- Author: TheBB
+findCompletion :: String -> StateM [Completion]
+findCompletion s = do map simpleCompletion . filter (s `isPrefixOf`) . wordList <$> get
 
+hlSettings :: Settings (StateT State IO)
+hlSettings = setComplete (completeWord Nothing " \t" findCompletion) defaultSettings
+
+main :: IO ((), State)
+main = runStateT (runInputT hlSettings repl) initState
--- Code/REPL.hs
+++ Code/REPL.hs
@@ -4,11 +4,15 @@ import System.Console.Haskeline
 import Expr
 import Parsing
 import Control.Monad.IO.Class
+import Control.Monad.Trans (lift)
+import Control.Monad.State.Strict (get, put, runStateT, StateT)
 
-data State = State {vars :: BTree, commands :: [String]}
+type StateM = StateT State IO
+type InputM = InputT StateM
+data State = State {vars :: BTree, commands :: [String], wordList :: [String]}
 
 initState :: State
-initState = State Leaf []
+initState = State Leaf [] ["False", "True", "else", "if", "import", "print", "quit", "then", "toFloat(", "toInt(", "toString(", "while"]
 
 -- Given a variable name and a value, return a new set of variables with
 -- that name and value added.
@@ -35,7 +39,7 @@ dropVar _name (Node (name, value) ltree rtree)
             Node (name_, value_) _ Leaf -> (name_, value_)
             Node (_, _) _ rtree -> largestVar rtree
 
-process :: State -> Command -> InputT IO (State)
+process :: State -> Command -> InputT StateM State
 process st (Set var e) =
   do
     case eval (vars st) e of
@@ -57,56 +61,62 @@ process st (Print e) =
     -- Print the result of evaluation
     return st
 
-processRepl :: State -> Command -> InputT IO ()
-processRepl st (Set var e)  = do st' <- process st (Set var e)
-                                 repl st'
-processRepl st (Print e)    = do st' <- process st (Print e)
-                                 repl st'
-processRepl st (Quit)       = outputStrLn "Bye"
-processRepl st (Import filepath) = do text <- liftIO (readFile filepath)
-                                      repl (st {commands = lines text ++ commands st})
-processRepl st (If e b1 b2) = case eval (vars st) e of
-  Just (BoolVal True)  -> do st' <- processBlock st b1
-                             repl st'
-  Just (BoolVal False) -> do st' <- processBlock st b2
-                             repl st'
-  _                    -> outputStrLn "Invalid boolean value"
-processRepl st (While e block) = loop st block e
-  where loop :: State -> [Command] -> Expr -> InputT IO ()
-        loop st cmds expr = case eval (vars st) expr of
-          Just (BoolVal True)  -> do st' <- processBlock st cmds
-                                     loop st' cmds expr
-          Just (BoolVal False) -> repl st
-          _                    -> outputStrLn "Invalid boolean value"
+-- processRepl :: State -> Command -> InputT IO ()
+-- processRepl st (Set var e)  = do st' <- process st (Set var e)
+--                                  repl st'
+-- processRepl st (Print e)    = do st' <- process st (Print e)
+--                                  repl st'
+-- processRepl st (Quit)       = outputStrLn "Bye"
+-- processRepl st (Import filepath) = do text <- liftIO (readFile filepath)
+--                                       repl (st {commands = lines text ++ commands st})
+-- processRepl st (If e b1 b2) = case eval (vars st) e of
+--   Just (BoolVal True)  -> do st' <- processBlock st b1
+--                              repl st'
+--   Just (BoolVal False) -> do st' <- processBlock st b2
+--                              repl st'
+--   _                    -> outputStrLn "Invalid boolean value"
+-- processRepl st (While e block) = loop st block e
+--   where loop :: State -> [Command] -> Expr -> InputT IO ()
+--         loop st cmds expr = case eval (vars st) expr of
+--           Just (BoolVal True)  -> do st' <- processBlock st cmds
+--                                      loop st' cmds expr
+--           Just (BoolVal False) -> repl st
+--           _                    -> outputStrLn "Invalid boolean value"
 
 
-processBlock :: State -> [Command] -> InputT IO(State)
-processBlock st (cmd: [])   = do st' <- process st cmd
-                                 return st'
-processBlock st (cmd: cmds) = do st' <- process st cmd
-                                 processBlock st' cmds
-processBlock st _           = return st
+-- processBlock :: State -> [Command] -> InputT IO(State)
+-- processBlock st (cmd: [])   = do st' <- process st cmd
+--                                  return st'
+-- processBlock st (cmd: cmds) = do st' <- process st cmd
+--                                  processBlock st' cmds
+-- processBlock st _           = return st
 
 -- Read, Eval, Print Loop
 -- This reads and parses the input using the pCommand parser, and calls
 -- 'process' to process the command.
 -- 'process' will call 'repl' when done, so the system loops.
 
-repl :: State -> InputT IO ()
-repl st = do outputStrLn ("Variables: " ++ show (vars st)) -- TODO: debug message, to be removed in the future
-             inp <- case commands st of
-               [] -> getInputLine ("> ")
-               (x:xs) -> return (Just x)
-             -- inp <- getLine
-             case inp of
-                Nothing    -> return ()
-                Just input -> 
-                    case parse pStatement input of
-                        [(cmd, "")] -> -- Must parse entire input
-                                processRepl st' cmd
-                        _ -> do outputStrLn "Parse error"
-                                repl st'
-             where st' = case commands st of
-                              [] -> st
-                              (x:xs) -> st {commands = xs}
-
+repl :: InputM ()
+repl = do st <- lift get
+          outputStrLn ("Variables: " ++ show (vars st)) -- TODO: debug message, to be removed in the future
+          inp <- case commands st of
+            [] -> getInputLine ("> ")
+            (x:xs) -> do lift $ put st {commands = xs}
+                         return (Just x)
+          -- inp <- getLine
+          case inp of
+            Nothing    -> return ()
+            Just input -> 
+                case parse pStatement input of
+                    [(cmd, "")] -> -- Must parse entire input
+                      case cmd of
+                        Quit -> do outputStrLn "Bye"
+                                   return ()
+                        (Set var e) -> do st' <- process st cmd
+                                          lift $ put st' {wordList = var : wordList st'}
+                                          repl
+                        _ -> do st' <- process st cmd
+                                lift $ put st'
+                                repl
+                    _ -> do outputStrLn "Parse error"
+                            return ()
